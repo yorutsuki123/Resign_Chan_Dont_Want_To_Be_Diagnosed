75d7d2d5915fe926a98c339d36af2dd1598a514b
Chaos <cxz2235@gmail.com>
2022-01-26 21:58:58 +0800

    changeColor

--- Assets/Script/ChessGrid.cs
+++ Assets/Script/ChessGrid.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public enum Status
 {
     safe, // 安全
@@ -11,9 +11,30 @@ public enum Status
 
 public class ChessGrid : MonoBehaviour
 {
+    public Sprite[] spriteArray = new Sprite[3];
+    
     
     // 宣告你的列舉變數 遊戲狀態
     public Status status;
+
+    public void changeColor(string color)
+    {
+        if (color == "green")
+        {
+            GetComponent<Image>().sprite = spriteArray[(int)Status.safe];
+        }
+        else if (color == "red")
+        {
+            GetComponent<Image>().sprite = spriteArray[(int)Status.infect];
+        }
+        else
+        {
+            GetComponent<Image>().sprite = spriteArray[(int)Status.pending];
+        }
+
+    }
+
+
 
 
     public void diffuse()
