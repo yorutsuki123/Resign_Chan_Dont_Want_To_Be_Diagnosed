a1bbaaa676be0dc96ab60e93ddd79e27fc749eb0
Yorutsuki123 <yorutsuki123@gmail.com>
2022-01-26 20:44:18 +0800

    Queue

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -6,10 +6,29 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager gameManager;
 
-
     public Grid[,] gridArray = new Grid[10, 8];
     public List<Grid> infectList = new List<Grid>();
-    public List<Grid> pendingQueue = new List<Grid>();
+    public Queue<Grid> pendingQueue = new Queue<Grid>();
+
+    void pendingPush(Grid grid)
+    {
+        if (pendingQueue.Count >= 3)
+            return;
+        pendingQueue.Enqueue(grid);
+    }
+
+    Grid pendingPop()
+    {
+        if (pendingQueue.Count == 0)
+            return null;
+        return pendingQueue.Dequeue();
+    }
+
+    void setStatus(int x, int y, Status newStatus)
+    {
+        
+    }
+
     // Start is called before the first frame update
     void Awake()
     {
--- Assets/Script/Grid.cs
+++ Assets/Script/Grid.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Status
+{
+    safe, // 安全
+    infect, // 感染
+    pending   // 待處理
+}
+
 public class Grid : MonoBehaviour
 {
-    enum Status
-    {
-        safe, // 安全
-        infect, // 感染
-        pending   // 待處理
-    }
+    
 
     // 宣告你的列舉變數 遊戲狀態
     Status status;
