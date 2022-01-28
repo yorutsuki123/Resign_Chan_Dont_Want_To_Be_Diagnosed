8e3e5c09e332cef225a5214eb2145760a42d2421
Chaos <cxz2235@gmail.com>
2022-01-28 13:37:34 +0800

    fix Gameqte

--- Assets/Script/Gameqte.cs
+++ Assets/Script/Gameqte.cs
@@ -1,96 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Gameqte : MonoBehaviour {  
     public Queue<char> Qtekey = new Queue<char>();
     public string chara = "wsad";
-    public int rand = Random.Range(0, 4);
+    public char qteCompare='?';
+    public int rand = 0;
     public int tmp = 0;
-    public int tf = 0;
+    public int tf = 3;
+    public Text qteText;
     public void Qtemethod() {
         
-        if(tmp != GameManager.gameManager.pendingCount){
-            if(tmp > GameManager.gameManager.pendingCount && GameManager.gameManager.pendingCount != 0 || (tmp == 0 && GameManager.gameManager.pendingCount == 1)){
-                for (int i = 0; i <= 3; i++) {
-                    Qtekey.Enqueue(chara[rand]);
-                }
+        if(Qtekey.Count==0)
+        {
+            for (int i = 0; i <= 3; i++) {
+                rand = Random.Range(0, 4);
+                Qtekey.Enqueue(chara[rand]);
             }
-            
-            tmp = GameManager.gameManager.pendingCount;
+            foreach (char q in Qtekey)
+            {
+                qteText.text += q;
+            }
         }
-        
-
-
         if (GameManager.gameManager.pendingCount != 0) {
-            
-
-            if(Qtekey.Count != 0) {   
+            if(Qtekey.Count != 0) {
                 if (Input.GetKeyDown(KeyCode.W)) {
-                    if (Qtekey.Peek().Equals('w')) {
-                        tf = 1;
-                        Qtekey.Dequeue();
-                        //continue;
-                    }
-                    else{
-                        tf = 0;
-                        //break;
-                    }
+                    qteCompare = 'w';
                 }
-
                 else if (Input.GetKeyDown(KeyCode.S)) {
-                    if (Qtekey.Peek().Equals('s')) {
-                        tf = 1;
-                        Qtekey.Dequeue();
-                        //continue;
-                    }
-                    else{
-                        tf = 0;
-                        //break;
-                    }
+                    qteCompare = 's';
                 }
-
                 else if (Input.GetKeyDown(KeyCode.A)) {
-                    if (Qtekey.Peek().Equals('a')) {
-                        tf = 1;
-                        Qtekey.Dequeue();
-                        //continue;
-                    }
-                    else{
-                        tf = 0;
-                        //break;
-                    }
+                    qteCompare = 'a';
                 }
-
                 else if (Input.GetKeyDown(KeyCode.D)) {
-                    if (Qtekey.Peek().Equals('d')) {
-                        tf = 1;
-                        Qtekey.Dequeue();
-                        //continue;
-                    }
-                    else{
-                        tf = 0;
-                        //break;
-                    }
+                    qteCompare = 'd';
                 }
-                else{
-                    tf = 0;
-                    //break;
-                }
-                
-            }
-
-            if(tf == 0){
-                while(Qtekey.Count != 0) {
+                if(qteCompare == Qtekey.Peek())
+                {
+                    tf=1;
                     Qtekey.Dequeue();
+                    qteCompare = '?';
                 }
-                GameManager.gameManager.pendingPop(false);
-                return;
+                else 
+                {
+                    if(qteCompare != '?')
+                    {
+                        Qtekey.Clear();
+                        tf=0;
+                        GameManager.gameManager.pendingPop(false);
+                        qteText.text="";
+                        qteCompare = '?';
+                    }
+
+                }
             }
-            else if(Qtekey.Count == 0 && tf == 1){
+            if(Qtekey.Count == 0){
                 GameManager.gameManager.pendingPop(true);
+                Qtekey.Clear();
+                qteText.text="";
             }
-            
         }
     }
     void Update() {
